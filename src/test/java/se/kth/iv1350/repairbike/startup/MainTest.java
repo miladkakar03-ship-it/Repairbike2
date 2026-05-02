9a463dec36914ddbf950a758f4bfd907f72c3edd
miladkakar03-ship-it <miladkakar03@gmail.com>
2026-05-02 14:19:15 +0200

    hej

--- src/test/java/se/kth/iv1350/repairbike/startup/MainTest.java
+++ src/test/java/se/kth/iv1350/repairbike/startup/MainTest.java
@@ -2,6 +2,11 @@ package se.kth.iv1350.repairbike.startup;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Testar programmets startpunkt för att säkerställa att hela 
+ * applikationen kan initieras och köras utan fel.
+ */
+
 public class MainTest {
 
     @Test
